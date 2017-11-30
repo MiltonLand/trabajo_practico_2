e89f11ff89bff809d882be100b37d1d64a8950ef
Milton Landriel <milton_landriel@hotmail.com>
2017-11-30 19:55:54 -0300

    first commit

---
+++ tp2_milton_landriel/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp2_milton_landriel
+{
+    public class Class1
+    {
+    }
+}
